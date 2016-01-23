58d975e83b9103a36173f88c233b5f9913d90367
desperauxq <souceif@yandex.com>
2016-01-23 15:16:27 +0800

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Authored by: Desperauxq
+
 import SimpleHTTPServer
 import SocketServer
 
+# PORT location, I choose 8000 as usual
 PORT = 8000
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
