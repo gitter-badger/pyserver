171b34e292f873aa91f92e7caf95a3dc73e2eccf
desperauxq <souceif@yandex.com>
2016-01-23 15:13:39 +0800

    Created main.py. I also added a few comments in the top of the file

---
+++ main.py
@@ -0,0 +1,12 @@
+# Authored by: Desperauxq
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "Serving at port", PORT
+httpd.serve_forever()
